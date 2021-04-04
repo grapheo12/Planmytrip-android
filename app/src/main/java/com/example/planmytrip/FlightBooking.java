b129427f1303f721e188df72ec388682159dd619
somnathjena2011 <somnathjena.2011@gmail.com>
2021-04-05 02:01:23 +0530

    flight results not done

---
+++ app/src/main/java/com/example/planmytrip/FlightBooking.java
@@ -0,0 +1,14 @@
+package com.example.planmytrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlightBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_booking);
+    }
+}
