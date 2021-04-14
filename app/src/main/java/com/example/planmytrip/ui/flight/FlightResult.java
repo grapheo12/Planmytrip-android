c3406250beb82aa1852b43a840e566d92b7bc436
somnathjena2011 <somnathjena.2011@gmail.com>
2021-04-14 22:22:40 +0530

    flight results

---
+++ app/src/main/java/com/example/planmytrip/ui/flight/FlightResult.java
@@ -0,0 +1,42 @@
+package com.example.planmytrip.ui.flight;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class FlightResult {
+    public int flight_id;
+    public String airlines;
+    public String departure;
+    public String seat_type;
+    public double fare;
+    public int seats;
+
+    public FlightResult(JSONObject object)
+    {
+        try {
+            this.flight_id = object.getInt("flight_id");
+            this.airlines = object.getString("airlines");
+            this.departure = object.getString("departure");
+            this.fare = object.getDouble(("fare"));
+            this.seat_type = object.getString("seat_type");
+            this.seats = object.getInt("seats");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+    public static ArrayList<FlightResult> fromJson(JSONArray jsonObjects) {
+        ArrayList<FlightResult> FlightResults = new ArrayList<FlightResult>();
+
+        for (int i = 0; i < jsonObjects.length(); i++) {
+            try {
+                FlightResults.add(new FlightResult(jsonObjects.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return FlightResults;
+    }
+}
---
+++ app/src/main/java/com/example/planmytrip/ui/flight/ItemFlight.java
@@ -0,0 +1,16 @@
+package com.example.planmytrip.ui.flight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.planmytrip.R;
+
+public class ItemFlight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_flight);
+    }
+}
