1c91156289d3215d2ec5a07a0adf66e03c75cdbe
somnathjena2011 <somnathjena.2011@gmail.com>
2021-04-15 09:04:57 +0530

    flight front

--- app/src/main/java/com/example/planmytrip/ui/flight/FlightFragment.java
+++ app/src/main/java/com/example/planmytrip/ui/flight/FlightFragment.java
@@ -1,10 +1,17 @@
 package com.example.planmytrip.ui.flight;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,70 +23,87 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.planmytrip.GlobalCtx;
 import com.example.planmytrip.MainActivity;
 import com.example.planmytrip.R;
+import com.example.planmytrip.ui.hotel.HotelViewModel;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
 
-public class FlightFragment extends AppCompatActivity {
+public class FlightFragment extends Fragment {
 
     Button searchBtn;
     EditText From, To, Doj, Tclass, Nop;
     private final String flightSearchRoute = "auth/flight";
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_flight);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_flight, container, false);
+        System.out.println(root.toString());
 
-        From = (EditText)findViewById(R.id.from);
-        To = (EditText)findViewById(R.id.to);
-        Doj = (EditText)findViewById(R.id.doj);
-        Tclass = (EditText)findViewById(R.id.tclass);
-        Nop = (EditText)findViewById(R.id.nop);
+        return root;
     }
 
-    public void searchFlight(View view) throws JSONException {
-        String from = From.getText().toString();
-        String to = To.getText().toString();
-        String doj = Doj.getText().toString();
-        String tclass = Tclass.getText().toString();
-        int nop = Integer.parseInt(Nop.getText().toString());
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //setContentView(R.layout.fragment_flight);
 
-        JSONObject req = new JSONObject();
-        req.put("from", from);
-        req.put("to", to);
-        req.put("date_of_journey", doj);
-        req.put("ticket_class", tclass);
-        req.put("nop", nop);
+        From = (EditText)view.findViewById(R.id.from);
+        To = (EditText)view.findViewById(R.id.to);
+        Doj = (EditText)view.findViewById(R.id.doj);
+        Tclass = (EditText)view.findViewById(R.id.tclass);
+        Nop = (EditText)view.findViewById(R.id.nop);
+        searchBtn = (Button)view.findViewById(R.id.search);
 
-        String flightSearchUrl = GlobalCtx.urlPrefix + flightSearchRoute;
-        JsonObjectRequest flightSearchRequest = new JsonObjectRequest(Request.Method.GET, flightSearchUrl,
-                req, new Response.Listener<JSONObject>() {
+        searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(JSONObject response) {
-                Toast.makeText(getApplicationContext(),
-                        "Search Successful",
-                        Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                String from = From.getText().toString();
+                String to = To.getText().toString();
+                String doj = Doj.getText().toString();
+                String tclass = Tclass.getText().toString();
+                int nop = Integer.parseInt(Nop.getText().toString());
 
-                Intent resultPage = new Intent(FlightFragment.this, FlightBooking.class);
-                Bundle args = new Bundle();
-                args.putSerializable("response",(Serializable)response);
-                resultPage.putExtra("myKey",args);
-                FlightFragment.this.startActivity(resultPage);
-                FlightFragment.this.finish();
-                GlobalCtx.flightResult = response;
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(),
-                        "Something went wrong :-(",
-                        Toast.LENGTH_LONG).show();
+                JSONObject req = new JSONObject();
+                try {
+                    req.put("from", from);
+                    req.put("to", to);
+                    req.put("date_of_journey", doj);
+                    req.put("ticket_class", tclass);
+                    req.put("nop", nop);
+                }catch(JSONException e)
+                {
+                    e.printStackTrace();
+                }
+
+                String flightSearchUrl = GlobalCtx.urlPrefix + flightSearchRoute;
+                JsonObjectRequest flightSearchRequest = new JsonObjectRequest(Request.Method.GET, flightSearchUrl,
+                        req, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //Toast.makeText(this, "Search Successful", Toast.LENGTH_SHORT).show();
+
+                        Activity context = getActivity();
+                        Intent resultPage = new Intent(context, FlightBooking.class);
+                        Bundle args = new Bundle();
+                        args.putSerializable("response",(Serializable)response);
+                        resultPage.putExtra("myKey",args);
+                        FlightFragment.this.startActivity(resultPage);
+                        //FlightFragment.this.finish();
+                        GlobalCtx.flightResult = response;
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Toast.makeText(getApplicationContext(),"Something went wrong :-(",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                GlobalCtx.queue.add(flightSearchRequest);
             }
         });
-
-        GlobalCtx.queue.add(flightSearchRequest);
     }
 }
